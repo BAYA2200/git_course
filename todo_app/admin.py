ac08e2bb397cc4bfd0f42a222de2f37f1206b8be
BAYA <bayaman2101@gmail.com>
2022-10-06 20:24:57 +0600

    Before Uploud

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Todo
+
 admin.site.register(Todo)
+
+
 # Register your models here
 
-class TodoAdmin(admin.ModelAdmin):
-    pass
 
-admin.site.register(Todo, TodoAdmin)
+
+
